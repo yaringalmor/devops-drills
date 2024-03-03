8a2406e87b9f0bf7ccdfbb770b721886bbe19ce5
yaringalmor <yarin_galmor@yahoo.com>
2024-03-03 23:28:06 +0200

    Adding solutions for lesson3 exercise

---
+++ lesson3/drill_3.py
@@ -0,0 +1,16 @@
+"""
+Author: Yarin Galmor
+Description: Python Basics, Lesson 2, Drill 3
+Flow Control, Loops and Functions.
+"""
+
+
+def main():
+    try:
+        x = 1
+    finally:
+        print("finally")  # Code is legal.
+
+
+if __name__ == '__main__':
+    main()
