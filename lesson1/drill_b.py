5a63c32ba4b2f7c16a7d138030f17ba98d4e1026
yaringalmor <yarin_galmor@yahoo.com>
2024-03-03 21:52:26 +0200

    Adding solutions for lesson1 exercise

---
+++ lesson1/drill_b.py
@@ -0,0 +1,15 @@
+"""
+Author: Yarin Galmor
+Description: Python Basics, Lesson 1, Drill B
+Setting variables and practice variables operators.
+"""
+
+a = 8
+a = 17
+a = 9
+b = 6
+c = a + b
+b = c + a
+b = 8
+
+print(f"Here are the final value:\na - {a}, b - {b}, c - {c}")
---
+++ lesson1/drill_d.py
@@ -0,0 +1,9 @@
+"""
+Author: Yarin Galmor
+Description: Python Basics, Lesson 1, Drill D
+Setting variables and practice variables operators.
+"""
+
+x = 5
+y = 2.36
+print(x + int(y))
